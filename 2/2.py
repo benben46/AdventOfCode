ad7c394a52ba19b0dd00f50bc04449aa502ddcd3
benpu <pwc123451@gmail.com>
2024-04-17 11:39:10 -0400

    Worked on Day 2 but having issues with the spaces...parsing

--- 2/2.py
+++ 2/2.py
@@ -1,6 +1,30 @@
-sentence = "13dancingmelody13"
-num_values_only = [int(i) for i in list(sentence) if i.isdigit()]
-print(num_values_only)
+# Day 2: Cube Conundrum
 
+allowed = {"red": 12,
+           "green": 13,
+           "blue": 14}
 
-wat = list(sentence)
+# [12 green, 9 blue, 13 red]
+def isValid(set_score):
+    for element in set_score:
+        color = element.split(" ")[1].strip()
+        num = int(element.split(" ")[0]).strip()
+        if (allowed[color.lower()] < num):
+            return False
+    return True
+        
+possible_games = []
+with open("input.txt", "r") as f:
+    for line in f:
+        game_index = 0
+        # Remove the "Game #: " part
+        games = line.split(":", 1)[1]
+        sets = games.split(";")
+        stripped_sets = [x.strip() for x in sets]
+        for set in stripped_sets:
+            set_stats = set.split(",")
+            if isValid(set_stats):
+                possible_games.append(game_index)
+        game_index += 1
+
+print("Sum of Game #'s that qualify as possible games:", sum(possible_games))
